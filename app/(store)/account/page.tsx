6e49c76a23807212444ce1a5ecdfb34069e8f86c
mani agah esmaeilzad <mani.agah86@gmail.com>
2025-10-18 02:21:21 +0330

    change account file

--- app/(store)/account/page.tsx
+++ app/(store)/account/page.tsx
@@ -1,7 +1,9 @@
-import AccountDashboard from './AccountDashboard';
-import { getUserDashboard } from '@/lib/data';
+// app/account/page.tsx
+import AccountDashboard from "./AccountDashboard";
 
 export default async function AccountPage() {
+  const { getUserDashboard } = await import("@/lib/data");
   const dashboard = await getUserDashboard();
+
   return <AccountDashboard dashboard={dashboard} />;
 }
